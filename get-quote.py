d5b60ab045f5f5cd0d8f4934c0663972ccaac358
Kyle Roddick <kyle.roddick@gmail.com>
2021-06-30 17:28:15 -0300

    finish task list

--- get-quote.py
+++ get-quote.py
@@ -4,13 +4,33 @@ import random
 def primary():
     # print("Keep it logically awesome.")
 
-    f = open("quotes.txt")
+    noList = ['N', 'n', 'NO', 'no', 'No']
+    yesList = ['Y', 'y', 'YES', 'yes', 'Yes']
+    while True:
+        inp = input('Add a new quote? (Y/N): ')
+        if inp in noList:
+            break
+        elif inp in yesList:
+            f = open("quotes.txt", 'a')
+            nextQuote = input('New quote: ')
+            nextQuote = nextQuote + '\n'
+            f.write(nextQuote)
+            f.close()
+        else:
+            print('Input not recognised!')
+
+    f = open("quotes.txt", 'r')
     quotes = f.readlines()
     f.close()
 
     last = len(quotes) - 1
     rnd = random.randint(0, last)
-    print(quotes[rnd])
+    rnd1 = rnd
+    while rnd1 == rnd:
+        rnd1 = random.randint(0, last)
+    q0 = quotes[rnd]
+    q1 = quotes[rnd1].strip()
+    print(q0, q1)
 
 
 if __name__ == "__main__":
